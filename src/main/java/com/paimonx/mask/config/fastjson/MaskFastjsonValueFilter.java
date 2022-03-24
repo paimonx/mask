f766bbee382de3a5af90b4b6312ddd0da0474a83
xu <liu_zhaoxu@163.com>
2022-03-24 22:36:26 +0800

    支持fastjson

---
+++ src/main/java/com/paimonx/mask/config/fastjson/MaskFastjsonValueFilter.java
@@ -0,0 +1,15 @@
+package com.paimonx.mask.config.fastjson;
+
+import com.alibaba.fastjson.serializer.ValueFilter;
+
+/**
+ * @author xu
+ * @date 2022/3/23
+ */
+public class MaskFastjsonValueFilter implements ValueFilter {
+    // TODO: 2022/3/23  
+    @Override
+    public Object process(Object object, String name, Object value) {
+        return null;
+    }
+}
