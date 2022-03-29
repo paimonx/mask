777172c40057d90e32647dba1d70a9ee2d784bd5
xu <liu_zhaoxu@163.com>
2022-03-29 22:21:08 +0800

    删除及其影响效率的判断

--- src/main/java/com/paimonx/mask/config/jackson/MaskBeanPropertyFilter.java
+++ src/main/java/com/paimonx/mask/config/jackson/MaskBeanPropertyFilter.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.ser.PropertyWriter;
 import com.paimonx.mask.MaskConfigProperties;
 import com.paimonx.mask.MaskManager;
 import com.paimonx.mask.config.MaskSerializeTemplate;
-import com.paimonx.mask.util.EmptyUtils;
 
 /**
  * @author xu
@@ -22,21 +21,18 @@ public class MaskBeanPropertyFilter extends MaskSerializeTemplate {
 
     @Override
     public void serializeAsField(Object pojo, JsonGenerator jgen, SerializerProvider provider, PropertyWriter writer) throws Exception {
-        if (EmptyUtils.isNotEmpty(pojo)) {
-            // 当前key
-            String fieldName = writer.getName();
-            // 当前序列化的pojo class
-            Class<?> clazz = pojo.getClass();
-            if (canMask(clazz, fieldName)) {
-                // 当前值
-                Object value = writer.getMember().getValue(pojo);
-                jgen.writeObjectField(fieldName, doMask(fieldName, value, clazz));
-            } else {
-                super.serializeAsField(pojo, jgen, provider, writer);
-            }
+        // 当前key
+        String fieldName = writer.getName();
+        // 当前序列化的pojo class
+        Class<?> clazz = pojo.getClass();
+        if (canMask(clazz, fieldName)) {
+            // 当前值
+            Object value = writer.getMember().getValue(pojo);
+            jgen.writeObjectField(fieldName, doMask(fieldName, value, clazz));
         } else {
             super.serializeAsField(pojo, jgen, provider, writer);
         }
+
     }
 
     @Override
