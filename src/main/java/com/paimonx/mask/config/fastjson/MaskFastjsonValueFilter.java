b1a2b4b29a0db3013992ea44ae334ba4dfb3cb21
xu <liu_zhaoxu@163.com>
2022-04-02 22:19:52 +0800

    兼容fastjson

--- src/main/java/com/paimonx/mask/config/fastjson/MaskFastjsonValueFilter.java
+++ src/main/java/com/paimonx/mask/config/fastjson/MaskFastjsonValueFilter.java
@@ -1,15 +1,27 @@
 package com.paimonx.mask.config.fastjson;
 
 import com.alibaba.fastjson.serializer.ValueFilter;
+import com.paimonx.mask.MaskManager;
+import com.paimonx.mask.config.jackson.MaskBeanPropertyFilter;
+import com.paimonx.mask.util.EmptyUtils;
 
 /**
  * @author xu
  * @date 2022/3/23
  */
-public class MaskFastjsonValueFilter implements ValueFilter {
-    // TODO: 2022/3/23  
+public class MaskFastjsonValueFilter extends MaskBeanPropertyFilter implements ValueFilter {
+
+    protected MaskFastjsonValueFilter(MaskManager maskManager) {
+        super(maskManager);
+    }
+
+    // TODO: 2022/3/23
     @Override
     public Object process(Object object, String name, Object value) {
-        return null;
+        String maskType = getMaskType(object.getClass(), name, null);
+        if (EmptyUtils.isNotEmpty(maskType)){
+            return doMask(value,maskType);
+        }
+        return value;
     }
 }
